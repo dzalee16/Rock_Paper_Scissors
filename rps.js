70dc010dfa300bf4a58a9e5d5df2076d4108c3d6
Mladen <mladen.jovic93@yahoo.com>
2020-05-18 11:56:22 +0200

    Add images and making some changes with html, css and JS

--- rps.js
+++ rps.js
@@ -1,78 +1,78 @@
 let playerScore = 0;
 let computerScore = 0;
 let round = 1;
-let pointsToWin = 5;
+let pointsToWin = 3;
 
-const resultsDiv = document.querySelector("#resultsBox");
-const score = document.createElement("p");
-const roundUpdate = document.createElement("p");
-const showDecision = document.createElement("p");
+const results = document.querySelector('#resultsBox');
+const div = document.querySelector('div');
+const para = document.createElement('p');
+const winner = document.createElement('p');
 
-const buttons = document.querySelectorAll("button")
-    buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      let playerSelection = button.id;
-      playRound(playerSelection);
-    }) ;
-  }) ;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click' , (e) => {
+    let playerSelection = button.id;
+    playRound(playerSelection);
+  });
+});
 
-function computerPlay() { 
-  let rps = ["rock", "paper", "scissors"];
+function computerPlay() {
+  let rps = ["rock", "paper" , "scissors"];
   return rps[Math.floor(Math.random() * rps.length)];
 }
 
 function resetRound() {
-  round = 0;
   playerScore = 0;
   computerScore = 0;
-};
+  round = 0;
+}
 
-function playRound(playerSelection){
-    let computerSelection = computerPlay();
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore++;
-        roundUpdate.textContent = "You win! Rock beats scissors.";
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        computerScore++;
-        roundUpdate.textContent = "You lose! Paper beats rock.";
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore++;
-        roundUpdate.textContent = "You win! Scissors beats paper.";
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore++;
-        roundUpdate.textContent = "You lose! Rock beats scissors.";
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore++;
-        roundUpdate.textContent = "You win! Paper beats rock.";
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore++;
-        roundUpdate.textContent = "You lose! Scissors beats paper.";
-    } else {
-        roundUpdate.textContent = "It's a tie!" + " You both picked " + playerSelection + ".";
-    };
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
+  if(playerSelection === "scissors" && computerSelection === "paper") {
+    results.textContent = "You win! Scissors beats paper!";
+    playerScore++;
+  } else if(playerSelection === "rock" && computerSelection === "scissors") {
+    results.textContent = "You win! Rock beats scissors";
+    playerScore++;
+  } else if(playerSelection === "paper" && computerSelection === "rock") {
+    results.textContent = "You win! Paper beats rock";
+    playerScore++;
+  } else if(playerSelection === "rock" && computerSelection === "paper") {
+    results.textContent = "You lose! Paper beats rock";
+    computerScore++;
+  } else if(playerSelection === "paper" && computerSelection === "scissors") {
+    results.textContent = "You lose! Scissors beats paper";
+    computerScore++;
+  } else if(playerSelection === "scissors" && computerSelection === "rock") {
+    results.textContent = "You lose! Rock beats scissors";
+    computerScore++;
+  } else {
+    results.textContent = "It's a tie!" + " You both picked " + playerSelection + ".";
+  }
 
+  if(playerScore < pointsToWin && computerScore < pointsToWin) {
+    para.textContent = "[Round] " + round + " [Player] : " + playerScore  + " " + "VS" + "  [Computer] : " + computerScore + " . ";
+    winner.textContent = "";
+  };
 
-    if (playerScore < pointsToWin && computerScore < pointsToWin){
-        score.textContent = 'Round ' + round + ' - You: ' + playerScore + "  |  Computer: " + computerScore;
-        showDecision.textContent = "";    
-    };
-    
-    if (computerScore == pointsToWin && playerScore < pointsToWin){
-        score.textContent = 'Round ' + round + ' - You: ' + playerScore + "  |  Computer: " + computerScore;
-        showDecision.textContent = "Computer wins, sorry!";
-        resetRound();
-    } else if (playerScore == pointsToWin && computerScore < pointsToWin){
-        score.textContent = 'Round ' + round + ' - You: ' + playerScore + "  |  Computer: " + computerScore;
-        showDecision.textContent = "You win, congratulations!";
-        resetRound();
-    } else if (playerScore == (pointsToWin - 1) && computerScore == (pointsToWin - 1)) {
-        score.textContent = 'Round ' + round + ' - You: ' + playerScore + "  |  Computer: " + computerScore;
-        showDecision.textContent = "Tie breaker!";
-    }; 
-  
-  resultsDiv.appendChild(roundUpdate);
-  resultsDiv.appendChild(score);
-  resultsDiv.appendChild(showDecision);
+  if (playerScore == pointsToWin && computerScore < pointsToWin) {
+      para.textContent = "Round " + round + " Player: " + playerScore + " vs Computer: " + computerScore + " . ";
+      winner.textContent = "CONGRADULATION!!! The Player is WINNER!!!";
+      resetRound();
+  } else if (playerScore < pointsToWin && computerScore == pointsToWin) {
+      para.textContent = "Round " + round + " Player: " + playerScore + " vs Computer: " + computerScore + " . ";
+      winner.textContent = "SORRY!!! The WINNER is Computer!";
+      resetRound();
+  } else if (playerScore == (pointsToWin -1) && computerScore == (pointsToWin -1)) {
+      para.textContent = "Round " + round + " Player: " + playerScore + " vs Computer: " + computerScore + " . ";
+      winner.textContent = "TIE BREAKER!!!";
+  };
+
+  div.appendChild(results);
+  results.appendChild(para);
+  results.appendChild(winner);
 
   round++;
-}; 
+
+}
